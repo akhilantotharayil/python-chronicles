7b1ebea084f805916d814328ef7637d685b0a0bb
Akhil Anto Tharayil <90147210+akhilantotharayil@users.noreply.github.com>
2025-10-16 15:55:13 +0530

    Create 6_ChecknameispresentOrnot.py

---
+++ 6_ChecknameispresentOrnot.py
@@ -0,0 +1,13 @@
+'''Write a program to finds out whether a given name is present in a list or not.'''
+
+
+l = ["Akhil","Sunny","Sandeep","Ritesh","Kaunian","Pranavi"] 
+
+name = input("Enter the name: ")
+
+if(name in l):
+    print("Your name is in the list")
+    print("Iplit team OG Support team")
+else:
+    print("Your name is not in the list")
+    
