f07fbbc73dcd368961888b881b1b7dbf03432dc7
Akhil Anto Tharayil <90147210+akhilantotharayil@users.noreply.github.com>
2025-10-16 15:55:40 +0530

    Create 6_CheckUserornot.py

---
+++ 6_CheckUserornot.py
@@ -0,0 +1,9 @@
+'''Write a program to find whether a given username contains less than 10 chracters or not'''
+
+username = input("Enter the username: ")
+
+if(len(username)< 10):
+    print("Your username contains less than 10 characters")
+
+else:
+    print("All is well!!!")
