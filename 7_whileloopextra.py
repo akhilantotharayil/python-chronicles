c6d4bd70cbee6ff3243097c2725214cc1d4aa399
Akhil Anto Tharayil <90147210+akhilantotharayil@users.noreply.github.com>
2025-10-24 14:39:54 +0530

    Create 7_whileloopextra.py

---
+++ 7_whileloopextra.py
@@ -0,0 +1,28 @@
+#Counting with while loop
+i = 1
+while(i<=5):
+    print("Number is ",i)
+    i = i+1 
+
+
+#Print squares using while loop
+i = 1
+while(i <=5):
+    print(i,"square is ",i*i)
+    i = i+1
+
+#Print numbers backward
+i = 5
+while(i>0):
+    print(i)
+    i = i-1
+
+#guessing game
+name = "Akhil"
+guess = ""
+
+while guess != name:
+    guess= input("Enter the guessed name: ")
+
+print("You guessed it right HABIBI!")
+
