3cb1092adb9d8cae78b96e9169fa7eff6875e613
Akhil Anto Tharayil <90147210+akhilantotharayil@users.noreply.github.com>
2025-10-24 14:40:19 +0530

    Create 7_ExtraPrograms.py

---
+++ 7_ExtraPrograms.py
@@ -0,0 +1,20 @@
+for i in range(1,6):
+    print("Number is ",i)
+
+
+
+fruits = ["Apple","Orange","Kela"]
+for  fruit in fruits:
+    print("I like ",fruit)
+
+
+for i in range(1,11):
+    print(i)
+
+
+for i in range(1,6):
+    print(i,"Square is ",i*i)
+
+n=input("Say my name: ")
+for i in n:
+    print(i)
