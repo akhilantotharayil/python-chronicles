c7da068e1226874d576f4afed93863394c5bf7bf
akhilantotharayil <akhilantotharayil@gmail.com>
2025-11-16 07:37:46 +0530

    Added date

--- Devops_funutils.py
+++ Devops_funutils.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 command = "ls -la"
 command = "df -h"
@@ -13,6 +14,12 @@ def system_space(command):
 def system_ram(command):
     print(os.system(command))
 
+def system_time():
+    return datetime.datetime.today()
+
+today = system_time()
+print(today)
+
 #system_directory("uptime")
-system_space("df -h")
+#system_space("df -h")
 #system_ram("free -h")
